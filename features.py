cacc954733194e3c88e499f86faae04058c81eb4
lavi rajput <121419564+lavirajput001@users.noreply.github.com>
2025-11-17 23:35:04 +0530

    Create features.py

---
+++ features.py
@@ -0,0 +1,8 @@
+# This project uses CNN, but file included for completeness
+# If infer.py needs it, it will work.
+
+import cv2
+import numpy as np
+
+def dummy_feature():
+    return np.zeros((1, 128))
